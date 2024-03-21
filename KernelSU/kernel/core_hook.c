4bf2a726d2ef4a5243849ad726fb9316d6bcb984
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-03-21 12:29:43 +0800

    KernelSU: update to 11675
    import from https://github.com/backslashxx/KernelSU/tree/11675
    import kernel folder only

--- KernelSU/kernel/core_hook.c
+++ KernelSU/kernel/core_hook.c
@@ -612,6 +612,9 @@ int ksu_handle_setuid(struct cred *new, const struct cred *old)
 	// try umount ksu temp path
 	try_umount("/debug_ramdisk", false, MNT_DETACH);
 	try_umount("/sbin", false, MNT_DETACH);
+	
+	// try umount /system/etc/hosts (for bindhosts)
+	try_umount("/system/etc/hosts", false, MNT_DETACH);
 
 	return 0;
 }
